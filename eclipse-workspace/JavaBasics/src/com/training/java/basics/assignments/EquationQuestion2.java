9f72cbeef3677c8b81920dda025a811644ee3be3
archamahira <archageeson@gmail.com>
2025-04-28 22:45:48 -0400

    fifth commit

---
+++ eclipse-workspace/JavaBasics/src/com/training/java/basics/assignments/EquationQuestion2.java
@@ -0,0 +1,28 @@
+package com.training.java.basics.assignments;
+
+/*
+ * to print the result of the following operations. a. -5 + 8 * 6 
+ * b. (55+9) % 9 
+ * c. 20 + -3*5 / 8 
+ * d. 5 + 15 / 3 * 2 - 8 % 3
+ * 
+ * 
+ * */
+public class EquationQuestion2 {
+
+	public static void main(String[] args) {
+		
+		int a = -5 + 8 * 6;
+		System.out.println("a: " + a);
+		
+		int b = (55+9) % 9 ;
+		System.out.println("b: " + b);
+		
+		double c = 20 + -3*5 / 8 ;
+		System.out.println("c: " + c);
+		
+		double d = 5 + 15 / 3 * 2 - 8 % 3;
+		System.out.println("d: " + d);
+	}
+
+}
